80956856a00f6b7ba96da99bc6b479a32ad4b3ab
yk-lab <www.yklab.net@gmail.com>
2019-02-15 02:59:02 +0900

    データベース設定

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE', database_path('database_todo_app.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
